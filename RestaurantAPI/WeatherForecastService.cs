3aa5f2a71557f66d95adb28e0f0063f65f33ebd0
defcon1one1 <altair74@gmail.com>
2022-10-18 01:15:34 +0200

    Add project files.

---
+++ RestaurantAPI/WeatherForecastService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantAPI
+{
+    public class WeatherForecastService : IWeatherForecastService
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+
+        public IEnumerable<WeatherForecast> Get(int results, int min, int max)
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, results).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(min, max),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
