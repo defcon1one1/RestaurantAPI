cce7c32e7203b7c1d6f2ef4cf05cceea1e431085
defcon1one1 <altair74@gmail.com>
2022-10-21 02:17:28 +0200

    Result sorting validation

--- RestaurantAPI/Models/Validators/RestaurantQueryValidator.cs
+++ RestaurantAPI/Models/Validators/RestaurantQueryValidator.cs
@@ -1,4 +1,5 @@
 ﻿using FluentValidation;
+using RestaurantAPI.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +11,7 @@ namespace RestaurantAPI.Models.Validators
     public class RestaurantQueryValidator : AbstractValidator<RestaurantQuery>
     {
         private readonly int[] allowedPageSizes = new[] { 5, 10, 15 };
+        private readonly string[] allowedSortByColumnNames = new[] { nameof(Restaurant.Name), nameof(Restaurant.Description), nameof(Restaurant.Category) };
         public RestaurantQueryValidator()
         {
             RuleFor(r => r.PageNumber).GreaterThanOrEqualTo(1);
@@ -20,6 +22,12 @@ namespace RestaurantAPI.Models.Validators
                     context.AddFailure("PageSize", $"PageSize must be in [{string.Join(", ", allowedPageSizes)}]");
                 }
             });
+            RuleFor(r => r.SortBy)
+                .Must(value => string.IsNullOrEmpty(value) || allowedSortByColumnNames.Contains(value))
+                .WithMessage($"SortBy is optional or must be in [{string.Join(", ", allowedSortByColumnNames)}]");
         }
+
+
+
     }
 }
